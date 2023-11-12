9ca4ea3ab3eed972541959542a9dffcadca2b47b
AhnYeonghoo <dksdudgn00@naver.com>
2023-11-12 13:43:58 +0900

    Initialized

---
+++ ch02/Hello/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("여러분, 안녕하세요?\n반갑습니다!");
+        }
+    }
+}
