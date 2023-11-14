7cbe5f56070a238e17f3ab5ce96fabcd0c756f5c
AhnYeonghoo <dksdudgn00@naver.com>
2023-11-14 14:00:00 +0900

    d

--- ch02/Hello/FloatConversion.cs
+++ ch02/Hello/FloatConversion.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hello
+namespace FloatConversion
 {
     internal class FloatConversion
     {
+        public static void MainFloatConversion(string[] args)
+        {
+            float a = 69.6875f;
+            Console.WriteLine(a);
+
+            double b = (double)a;
+            Console.WriteLine(b);
+
+            Console.WriteLine("69.6875 == b : {0}", 69.6875 == b);
+
+            float x = 0.1f;
+            double y = (double)x;
+            Console.WriteLine("hello World");
+        }
     }
 }
