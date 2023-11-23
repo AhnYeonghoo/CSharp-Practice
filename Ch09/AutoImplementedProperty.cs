d83b2b060be93f96a430ea6454e9fc994c908b30
AhnYeonghoo <dksdudgn00@naver.com>
2023-11-23 19:31:56 +0900

    Chapter09 Property

---
+++ Ch09/AutoImplementedProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AutoImplementedProperty
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "UnKnown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    
+}
---
+++ Ch09/InitOnly.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InitOnly
+{
+    class Transaction
+    {
+        public string From { get; init; }
+        public string To { get; init; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            
+                return $"{From,-10} -> {To,-10} : ${Amount}";
+            
+        }
+    }
+}
