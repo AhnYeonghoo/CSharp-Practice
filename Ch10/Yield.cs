44f4bec6b84945741c85b62d2812f38c584ac654
AhnYeonghoo <dksdudgn00@naver.com>
2023-11-27 10:33:01 +0900

    Generic

--- Ch10/Yield.cs
+++ Ch10/Yield.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace Ch10
+namespace Yield
 {
-    internal class Yield
+    class MyEnumerator
     {
+        int[] numbers = { 1, 2, 3, 4 };
+        public IEnumerator GetEnumerator()
+        {
+            yield return numbers[0];
+            yield return numbers[1];
+            yield return numbers[2];
+            yield break;
+            yield return numbers[3];
+        }
     }
 }
