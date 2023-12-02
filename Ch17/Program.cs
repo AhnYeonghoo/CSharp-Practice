cbe67424eea07961da7fb7fb6652cd94fb4ce822
AhnYeonghoo <dksdudgn00@naver.com>
2023-12-02 14:09:11 +0900

    Dynamic Type

---
+++ Ch17/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ch17
+{
+    internal class Program
+    {
+        static void MainProgram(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
